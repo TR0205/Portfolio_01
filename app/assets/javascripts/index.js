da0b84ecec9214f0ab9457302812595e2e4a4681
TR0205 <index0205@gmail.com>
2020-06-20 17:04:13 +0900

    heroku エラー解消テスト(Precompiling assets failed.)

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,3 +1,2 @@
 $('#article_lists').append("<%= escape_javascript(render 'article_list', object: @articles) %>");
-$("#view_more").replaceWith("<%= escape_javascript( 
-link_to_next_page(@articles, 'もっと見る', remote: true, id: 'view_more')) %>");
+$("#view_more").replaceWith("<%= escape_javascript(link_to_next_page(@articles, 'もっと見る', remote: true, id: 'view_more')) %>");
