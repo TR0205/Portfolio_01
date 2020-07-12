6271a60370b8337715489e859f8ce9c761db955d
對馬篤 <a.tsushima@MacBook-Pro.local>
2020-07-12 15:04:55 +0900

    ゲストログイン機能の変数修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -103,8 +103,8 @@ class UsersController < ApplicationController
     user = User.find_or_create_by!(
       email: 'guest@example.com',
       name: 'example_user'
-    ) do |user|
-      user.password = SecureRandom.urlsafe_base64
+    ) do |new_guest|
+      new_guest.password = SecureRandom.urlsafe_base64
     end
     session[:user_id] = user.id
     redirect_to('/')
