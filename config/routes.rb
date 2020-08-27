52fdd654c72448c912a23cfde981725f233e2211
對馬篤 <a.tsushima@MacBook-Pro.local>
2020-08-27 19:44:51 +0900

    ルートページを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'posts#index'
+  root 'users#login_form'
   get 'about' => 'home#about'
 
   get 'posts/search' => 'posts#search'
