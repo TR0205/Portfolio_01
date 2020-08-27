1ef3e9c7982d5592a80a15c55a2729e2de0f0a12
對馬篤 <a.tsushima@MacBook-Pro.local>
2020-08-27 20:31:46 +0900

    ルート変更：いいね、キャンセル

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -27,7 +27,7 @@ class LikesController < ApplicationController
       post_id: params[:post_id]
     )
     @like.save
-    redirect_to('/')
+    redirect_to('/posts/index')
   end
 
   def destroy_index
@@ -36,6 +36,6 @@ class LikesController < ApplicationController
       post_id: params[:post_id]
     )
     @like.destroy
-    redirect_to('/')
+    redirect_to('/posts/index')
   end
 end
